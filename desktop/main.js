161acd33602560dfd0f28a2e005cf58c4c760020
davidraketic45-rgb <davidraketic45@gmail.com>
2026-03-10 22:02:37 +0530

    Add files via upload

---
+++ desktop/main.js
@@ -0,0 +1,36 @@
+const { app, BrowserWindow, shell } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1280,
+    height: 800,
+    minWidth: 900,
+    minHeight: 600,
+    title: 'Spendly',
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    },
+    autoHideMenuBar: true
+  });
+
+  win.loadFile('index.html');
+
+  // Open external links in browser
+  win.webContents.setWindowOpenHandler(({ url }) => {
+    shell.openExternal(url);
+    return { action: 'deny' };
+  });
+}
+
+app.whenReady().then(() => {
+  createWindow();
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
